d2b72dd3ff04f17adc8be141f43e88593fc57658
md sumon mia <engineercse19@gmail.com>
2025-11-13 09:48:36 +0600

    some code added

--- index.js
+++ index.js
@@ -96,11 +96,11 @@ async function run() {
     });
 
     // // 🔍 Get single car details
-    // app.get('/cars/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const car = await carsCollection.findOne({ _id: new ObjectId(id) });
-    //   res.send(car);
-    // });
+    app.get('/cars/:id', async (req, res) => {
+      const id = req.params.id;
+      const car = await carsCollection.findOne({ _id: new ObjectId(id) });
+      res.send(car);
+    });
 
     // 🧍‍♀️ My Listings (Private)
     app.get('/my-listings', verifyJWT, async (req, res) => {
